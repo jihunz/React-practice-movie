ecca1a198a3a7eb359322652dc5b998bfa77ed0b
jihunz <thespringsong@gmail.com>
2022-02-22 16:52:44 +0900

    useEffect 동작 시점 특정화 실습

--- src/App.js
+++ src/App.js
@@ -4,18 +4,30 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setVal] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
   const onClick = () => setVal((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
 
   console.log("I render all the time");
 
-  const once = () => {
+  useEffect(() => {
     console.log("I run only once");
-  }
+  }, []);
 
-  useEffect(once, [])
+  useEffect(() => {
+    console.log(counter + " changes");
+  }, [counter]);
+
+  useEffect(() => {
+    if (keyword !== "" & keyword.length > 4) {
+      console.log("search for " + keyword);
+    }
+  }, [keyword]);
 
   return (
     <div className="App">
+      <input type="text" placeholder="search" value={keyword} onChange={onChange}></input>
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
