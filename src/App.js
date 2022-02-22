a0a5e57a8691ca01ee61d1ce2c3cda1e63faf063
jihunz <thespringsong@gmail.com>
2022-02-22 16:36:02 +0900

    useEffect 적용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setVal] = useState(0);
   const onClick = () => setVal((prev) => prev + 1);
 
-  console.log("render");
+  console.log("I render all the time");
+
+  const once = () => {
+    console.log("I run only once");
+  }
+
+  useEffect(once, [])
 
   return (
     <div className="App">
