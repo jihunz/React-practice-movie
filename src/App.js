c485cdaa75065a9995b7b524133293b49b35826c
jihunz <thespringsong@gmail.com>
2022-02-22 17:36:44 +0900

    Cleanup 기능 실습

--- src/App.js
+++ src/App.js
@@ -2,34 +2,26 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setVal] = useState(0);
-  const [keyword, setKeyword] = useState("");
-
-  const onClick = () => setVal((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  console.log("I render all the time");
-
+function Hello() {
+  
   useEffect(() => {
-    console.log("I run only once");
-  }, []);
-
-  useEffect(() => {
-    console.log(counter + " changes");
-  }, [counter]);
-
-  useEffect(() => {
-    if (keyword !== "" & keyword.length > 4) {
-      console.log("search for " + keyword);
+    console.log("hi");
+    return () => {
+      console.log("bye");
     }
-  }, [keyword]);
+  }, []);
+  return <h1>Hello</h1>
+}
+
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
 
   return (
     <div className="App">
-      <input type="text" placeholder="search" value={keyword} onChange={onChange}></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>show</button>
     </div>
   );
 }
