d15d4e5b4d0610b51f915b6cb90324cfccf87efe
jihunz <thespringsong@gmail.com>
2022-02-22 16:27:25 +0900

    counter 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setVal] = useState(0);
+  const onClick = () => setVal((prev) => prev + 1);
+
+  console.log("render");
+
   return (
     <div className="App">
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text="continue"/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
