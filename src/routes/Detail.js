3e67865ab7bf13a4dc94032d4a46d3435211cc28
jihunz <thespringsong@gmail.com>
2022-02-24 21:36:03 +0900

    영화 정보 출력

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,12 +1,14 @@
-import { useEffect } from "react";
-import {useParams} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function Detail() {
+    const [movie, setMovie] = useState([]);
     const { id } = useParams();
-    const getMovie = async() => {
+    const getMovie = async () => {
         const json = await (
             await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
         ).json();
+        setMovie(json.data.movie);
         console.log(json);
     }
 
@@ -15,7 +17,16 @@ function Detail() {
     }, []);
 
     return (
-        <h1>Detail</h1>
+        <>
+            <h1>Detail</h1>
+            <img src={movie.large_cover_image}></img>
+            <h3>{movie.title_long}</h3>
+            <p>{
+                movie.genres && movie.genres.map(g => (
+                    <span key={g}>{g} </span>
+                ))}
+            </p>
+        </>
     );
 }
 
