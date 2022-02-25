18920036b995a936df5f0a380f4a519314d455b2
jihunz <thespringsong@gmail.com>
2022-02-25 23:33:46 +0900

    로딩 메세지 추가

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -3,13 +3,14 @@ import { useParams } from "react-router-dom";
 
 function Detail() {
     const [movie, setMovie] = useState([]);
+    const [loading, setLoading] = useState(true);
     const { id } = useParams();
     const getMovie = async () => {
         const json = await (
             await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
         ).json();
         setMovie(json.data.movie);
-        console.log(json);
+        setLoading(false);
     }
 
     useEffect(() => {
@@ -17,16 +18,17 @@ function Detail() {
     }, []);
 
     return (
-        <>
+        <div>
             <h1>Detail</h1>
-            <img src={movie.large_cover_image}></img>
-            <h3>{movie.title_long}</h3>
-            <p>{
-                movie.genres && movie.genres.map(g => (
-                    <span key={g}>{g} </span>
-                ))}
-            </p>
-        </>
+                {loading ? <h2>loading...</h2> : null}
+                <img src={movie.large_cover_image}></img>
+                <h3>{movie.title_long}</h3>
+                <p>{
+                    movie.genres && movie.genres.map(g => (
+                        <span key={g}>{g} </span>
+                    ))}
+                </p>
+        </div>
     );
 }
 
